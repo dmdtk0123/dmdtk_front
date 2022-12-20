3a26351450838508b901d043294989ef19d4f5aa
GounKim <kgukgu2001@gmail.com>
2022-12-20 21:10:03 +0900

    logo link 수정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,30 +4,21 @@ import styled from "styled-components"
 import theme from "../styles/theme"
 import Logo from "../images/Logo"
 import Button from "../components/Button"
-import { Link } from 'react-router-dom'
-
-
-const HeaderWrapper = styled.header`
-    display: flex;
-    justify-content: space-between;
-    height: 60px;
-    width: 100%;
-    top: 0px;
-    font-size: ${theme.fontSize.lg};
-    font-weight: bold;
-    font-family: 'SCDream8';
-`
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+    let navigate = useNavigate();
     
+    const navigateToMainPage = () => {
+        navigate("/");
+    };
+
     return (
         <HeaderWrapper>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <div>
-                    <Logo width={40} height={30} color="#4C5D87" />
-                    <span>Maple</span>
-                </div>
-            </Link>
+            <div style={{cursor: "pointer"}} onClick={navigateToMainPage}>
+                <Logo width={40} height={30} color="#4C5D87" />
+                <span>Maple</span>
+            </div>
             <div>
                 <Button type="button" class="float-right" color="#4C5D87">
                     <span>Login</span>
@@ -39,5 +30,16 @@ const Header = () => {
         </HeaderWrapper>
     );
 }
+
+const HeaderWrapper = styled.header`
+    display: flex;
+    justify-content: space-between;
+    height: 60px;
+    width: 100%;
+    top: 0px;
+    font-size: ${theme.fontSize.lg};
+    font-weight: bold;
+    font-family: 'SCDream8';
+`
 
 export default Header
