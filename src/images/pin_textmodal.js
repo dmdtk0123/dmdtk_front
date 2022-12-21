ee8fa54610bcf1dddfc3ad8dc6ef14d564fe02ac
JiYeon-rhd <yjyshsj0217@gmail.com>
2022-12-21 11:08:31 +0900

    이미지 추가

---
+++ src/images/pin_textmodal.js
@@ -0,0 +1,29 @@
+
+const MainPageImg2 = (props) => {
+    return (
+        <svg
+            version="1.1"
+            id="UploadImage"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100%"
+            height="100%"
+            viewBox="0 0 1440 1852"
+            enableBackground="new 0 0 {width} {height}"
+            xmlSpace="preserve"
+            role='img'
+            >
+            <image
+                id="uploadimage"
+                width="1440"
+                height="1852"
+                x="0"
+                y="0"
+                href="https://user-images.githubusercontent.com/88575220/208803534-0b8f8b2c-ac3b-4671-95f1-6843b2b54616.png"
+            />
+        </svg>
+  );
+};
+
+export default MainPageImg2;
